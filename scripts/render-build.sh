e506bef95844c24012a25555fd7d7a773470a313
Tsion <tsiondemsew19@gmail.com>
2025-06-24 18:08:31 +0300

    update

--- scripts/render-build.sh
+++ scripts/render-build.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 composer install --no-interaction --prefer-dist
-php artisan config:cache
+php artisan config:cache 
+# Run database migrations
 php artisan migrate --force
+
+# Clear cache
+php artisan cache:clear
+php artisan view:clear
+php artisan config:clear
+
+# Optimize
+php artisan optimize
